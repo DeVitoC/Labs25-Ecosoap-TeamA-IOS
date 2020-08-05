a8156a926986ddb3ecb0d5d803394e8439d48946
Jon Bash <jonbash@protonmail.com>
2020-08-05 15:45:16 -0700

    set up AppFlowCoordinator

--- EcoSoapBank/App/SceneDelegate.swift
+++ EcoSoapBank/App/SceneDelegate.swift
@@ -10,8 +10,21 @@ import UIKit
 import OktaAuth
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
     var window: UIWindow?
+
+    var appCoordinator: FlowCoordinator?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+
+        window = UIWindow(windowScene: windowScene)
+        appCoordinator = AppFlowCoordinator(window: window!)
+        appCoordinator?.start()
+    }
     
     func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
         
