46ce9212f17b27229053e6f7c4ea45cc8c206204
Jon Bash <jonbash@protonmail.com>
2020-08-07 09:43:07 -0700

    add tests

---
+++ EcoSoapBankTests/UtilityTests.swift
@@ -0,0 +1,25 @@
+//
+//  UtilityTests.swift
+//  EcoSoapBankTests
+//
+//  Created by Jon Bash on 2020-08-07.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import XCTest
+@testable import EcoSoapBank
+
+
+class UtilityTests: XCTestCase {
+
+    func testConfigure() {
+    }
+
+//    func testPerformanceExample() throws {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
+
+}
