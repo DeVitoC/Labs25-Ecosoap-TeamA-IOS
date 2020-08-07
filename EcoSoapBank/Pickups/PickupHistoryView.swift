f4fb2a92b6152ccae0b0b7e8fdd6b2e62d63012a
Jon Bash <jonbash@protonmail.com>
2020-08-07 14:36:58 -0700

    add various pickup stuff

---
+++ EcoSoapBank/Pickups/PickupHistoryView.swift
@@ -0,0 +1,21 @@
+//
+//  PickupHistoryView.swift
+//  EcoSoapBank
+//
+//  Created by Jon Bash on 2020-08-07.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import SwiftUI
+
+struct PickupHistoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PickupHistoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        PickupHistoryView()
+    }
+}
