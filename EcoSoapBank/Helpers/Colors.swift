e60756f2048c6249c62988ba9e2ebd996201ea21
Shawn Gee <shawngee@gmail.com>
2020-08-10 21:39:28 -0400

    add colors.swift with UIColor extension for convenience, as well as corresponding unit test file

---
+++ EcoSoapBank/Helpers/Colors.swift
@@ -0,0 +1,18 @@
+//
+//  Colors.swift
+//  EcoSoapBank
+//
+//  Created by Shawn Gee on 8/10/20.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static let esbGreen = UIColor(named: "ESBGreen")!
+    static let downyBlue = UIColor(named: "DownyBlue")!
+    static let codGrey = UIColor(named: "CodGrey")!
+    static let montanaGrey = UIColor(named: "MontanaGrey")!
+    static let shuttleGrey = UIColor(named: "ShuttleGrey")!
+    static let silver = UIColor(named: "Silver")!
+}
