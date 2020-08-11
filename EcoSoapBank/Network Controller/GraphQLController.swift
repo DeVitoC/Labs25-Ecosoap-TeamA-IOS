c18e4ce26caddc2cd79a2a3f193f787efc549f97
Christopher DeVito <christopher.devito@protonmail.com>
2020-08-10 21:09:12 -0400

    Added MARKs, and separated out request property and json decoding into separate methods. Added Mutation request method.

--- EcoSoapBank/Network Controller/GraphQLController.swift
+++ EcoSoapBank/Network Controller/GraphQLController.swift
@@ -14,21 +14,31 @@ enum HTTPMethod: String {
 
 /// Class containing methods for communicating with GraphQL backend
 class GraphQLController {
-    let url = URL(string: "http://35.208.9.187:9094/ios-api-1/")!
+
+    // MARK: - Properties
+
+    private let url = URL(string: "http://35.208.9.187:9094/ios-api-1/")!
+    // Setting up the url request
+    private lazy var request: URLRequest = {
+        var request = URLRequest(url: url)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        return request
+    }()
+
+    // MARK: - Request methods
 
     /// Method for GraphQL query requests
     /// - Parameters:
-    ///   - query: The intended query as saved in string format
+    ///   - query: The intended query in string format
     ///   - session: The URLSession used for the request. By default this is URLSession.shared
     ///   - completion: Completion handler that passes back a Result of type Profile or Error
-    func queryRequest(query: String, session: URLSession = URLSession.shared, completion: @escaping (Result<Profile, Error>) -> Void) {
-        // The url request for the query
-        var request = URLRequest(url: url)
-        let body = ["query": query]
-
-        request.httpMethod = HTTPMethod.post.rawValue
+    func queryRequest(query: String,
+                      session: URLSession = URLSession.shared,
+                      completion: @escaping (Result<Profile, Error>) -> Void) {
+        // Add body to query request
+        let body: [String: String] = ["query": query]
         request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: [])
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
 
         session.dataTask(with: request) { data, _, error in
             if let error = error {
@@ -42,16 +52,54 @@ class GraphQLController {
                 return
             }
 
-            do {
-                // Decode data as ProfileQuery and pass the stored object of type Profile
-                let profileJSON = try JSONDecoder().decode(ProfileQuery.self, from: data)
-                let profile = Array(profileJSON.data.values)[0]
-                completion(.success(profile))
-            } catch {
+            completion(self.decodeJSON(data: data))
+        }.resume()
+    }
+
+    /// Method for GraphQL mutation requests
+    /// - Parameters:
+    ///   - mutationQuery: The indended mutation query in string format
+    ///   - variables: The variables to be passed in the request
+    ///   - session: The URLSession used for the request. By default this is URLSession.shared
+    ///   - completion: Completion handler that passes back a Result of type Prfile or Error
+    func mutationRequest(mutationQuery: String,
+                         variables: [Any] = [],
+                         session: URLSession = URLSession.shared,
+                         completion: @escaping (Result<Profile, Error>) -> Void) {
+        // Add body to mutate request
+        let body: [String: Any] = ["mutation": mutationQuery, "variables": variables]
+        request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: [])
+
+        session.dataTask(with: request) { data, _, error in
+            if let error = error {
                 NSLog("\(error)")
                 completion(.failure(error))
                 return
             }
+
+            guard let data = data else {
+                NSLog("Data is nil")
+                return
+            }
+
+            completion(self.decodeJSON(data: data))
         }.resume()
+    }
+
+    // MARK: - Helper Methods
+
+    /// Method to decode JSON Data to usable Type
+    /// - Parameter data: The JSON Data returned from the request
+    /// - Returns: Either an Error or the Decoded object
+    private func decodeJSON(data: Data) -> Result<Profile, Error> {
+        do {
+            // Decode data as ProfileQuery and pass the stored object of type Profile through completion
+            let json = try JSONDecoder().decode(ProfileQuery.self, from: data)
+            let profile = Array(json.data.values)[0]
+            return .success(profile)
+        } catch {
+            NSLog("\(error)")
+            return .failure(error)
+        }
     }
 }
