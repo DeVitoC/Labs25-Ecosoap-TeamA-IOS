eb3fe9b7c6df2a09c8824ed42b4734d4ab762c55
Christopher DeVito <christopher.devito@protonmail.com>
2020-08-12 14:36:09 -0400

    Removed QueryObjects from GraphQLQueries so they can be used for mutation queries also.

--- EcoSoapBank/Network Controller/GraphQLQueries.swift
+++ EcoSoapBank/Network Controller/GraphQLQueries.swift
@@ -16,7 +16,7 @@ enum GraphQLQueries {
     query ImpactStatsByPropertyIdInput($input: ID!) {
         impactStatsByPropertyId(input:$input) {
             impactStats {
-                \(impactStats)
+                \(QueryObjects.impactStats)
             }
         }
     }
@@ -26,7 +26,7 @@ enum GraphQLQueries {
     query UserByIdInput($input: ID!) {
         userById(input:$input) {
             user {
-                \(user)
+                \(QueryObjects.user)
             }
         }
     }
@@ -39,19 +39,19 @@ enum GraphQLQueries {
                 id
                 name
                 address {
-                    \(address)
+                    \(QueryObjects.address)
                 }
                 email
                 phone
                 coordinates {
-                    \(coordinates)
+                    \(QueryObjects.coordinates)
                 }
                 properties {
-                    \(property)
+                    \(QueryObjects.property)
                 }
                 workflow
                 impact {
-                    \(impactStats)
+                    \(QueryObjects.impactStats)
                 }
             }
         }
@@ -62,122 +62,11 @@ enum GraphQLQueries {
     query PropertiesByUserIdInput($input: ID!) {
         propertiesByUserId(input:$input) {
             properties {
-                \(property)
+                \(QueryObjects.property)
             }
         }
     }
     """
 
-    // MARK: - Query Objects
-    static let impactStats = """
-    soapRecycled
-    linensRecycled
-    bottlesRecycled
-    paperRecycled
-    peopleServed
-    womenEmployed
-    """
 
-    static let user = """
-    id
-    firstName
-    middleName
-    lastName
-    title
-    company
-    email
-    phone
-    skype
-    address {
-        \(address)
-    }
-    signupTime
-    properties {
-        \(property)
-    }
-    """
-
-    static let address = """
-    address1
-    address2
-    address3
-    city
-    state
-    postalCode
-    country
-    formattedAddress
-    """
-
-    // Property and Contract have an unresolved circular reference - required field
-    // Property and Pickup have an unresolved circular reference - required field
-    /* temporarily removed Contract from Property. if needed put this back in after users/pickups:
-     contract {
-     \(contract)
-     }
-     */
-    /* temporarily removed Pickup from Property. if needed put this back in after users:
-     pickups {
-     \(pickup)
-     }
-     */
-    static let property = """
-    id
-    name
-    propertyType
-    rooms
-    services
-    collectionType
-    logo
-    phone
-    billingAddress {
-        \(address)
-    }
-    shippingAddress {
-        \(address)
-    }
-    coordinates {
-        \(coordinates)
-    }
-    shippingNote
-    notes
-    hub {
-        \(hub)
-    }
-    impact {
-        \(impactStats)
-    }
-    users {
-        \(userWithOnlyRequiredFields)
-    }
-    """
-
-    static let coordinates = """
-    latitude
-    longitude
-    """
-
-    static let hub = """
-    id
-    name
-    address {
-        \(address)
-    }
-    email
-    phone
-    coordinates {
-        \(coordinates)
-    }
-    workflow
-    impact {
-        \(impactStats)
-    }
-    """
-
-    static let userWithOnlyRequiredFields = """
-    id
-    firstName
-    lastName
-    email
-    phone
-    """
 }
