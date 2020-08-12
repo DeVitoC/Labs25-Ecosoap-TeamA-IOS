af87085c533f5f64ee79db1b9ab9355bb104edd8
Shawn Gee <shawngee@gmail.com>
2020-08-12 08:01:35 -0400

    add collection view to impact vc

--- EcoSoapBank/Impact/ImpactViewController.swift
+++ EcoSoapBank/Impact/ImpactViewController.swift
@@ -10,19 +10,48 @@ import UIKit
 
 
 class ImpactViewController: UIViewController {
-    lazy var label = configure(UILabel()) {
-        $0.translatesAutoresizingMaskIntoConstraints = false
-        $0.text = "Hello impact"
-    }
+    
+    var collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
+        view.backgroundColor = .esbGreen
         
-        view.addSubview(label)
+        collectionView.register(ImpactCell.self, forCellWithReuseIdentifier: ImpactCell.reuseIdentifier)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .clear
+        
+        view.addSubview(collectionView)
         NSLayoutConstraint.activate([
-            label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            label.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
         ])
+        
+    }
+}
+extension ImpactViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        6
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ImpactCell.reuseIdentifier, for: indexPath) as? ImpactCell else {
+            fatalError("Could not cast cell as \(ImpactCell.self)")
+        }
+        cell.alignment = indexPath.row % 2 == 0 ? .leading : .trailing
+        
+        return cell
+    }
+
+}
+
+extension ImpactViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = view.frame.width
+        return CGSize(width: width, height: width * 0.32)
     }
 }
