1b5bd834ea25edb57d845c1c707470cdc79aed9c
Shawn Gee <shawngee@gmail.com>
2020-08-17 13:00:28 -0400

    add extension for filling superview with optional padding

---
+++ EcoSoapBank/Helpers/UIKit/UIView+Layout.swift
@@ -0,0 +1,29 @@
+//
+//  UIView+Layout.swift
+//  EcoSoapBank
+//
+//  Created by Shawn Gee on 8/17/20.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func fillSuperview(withPadding padding: UIEdgeInsets = .zero) {
+        assert(translatesAutoresizingMaskIntoConstraints == false,
+               "translatesAutoresizingMaskIntoConstraints must be set to false for view to use autolayout")
+        
+        guard let superview = superview else {
+            assertionFailure("View has no superview to fill")
+            return
+        }
+        
+        NSLayoutConstraint.activate([
+            self.leadingAnchor.constraint(equalTo: superview.leadingAnchor, constant: padding.left),
+            self.trailingAnchor.constraint(equalTo: superview.trailingAnchor, constant: padding.right),
+            self.topAnchor.constraint(equalTo: superview.topAnchor, constant: padding.top),
+            self.bottomAnchor.constraint(equalTo: superview.bottomAnchor, constant: padding.bottom),
+        ])
+    }
+}
