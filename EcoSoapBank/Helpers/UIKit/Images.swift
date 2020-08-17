47e5bf3369036cf83564b9674507d7a768761b50
Shawn Gee <shawngee@gmail.com>
2020-08-17 12:31:50 -0400

    add symbol for adding a box and nav bar background

---
+++ EcoSoapBank/Helpers/UIKit/Images.swift
@@ -0,0 +1,14 @@
+//
+//  Images.swift
+//  EcoSoapBank
+//
+//  Created by Shawn Gee on 8/17/20.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    static let addBoxSymbol = UIImage(named: "cube.box.plus")!
+    static let navBar = UIImage(named: "NavBar")
+}
