cc76c551989fbd4a61d008b697223f55e5a10c5e
Jon Bash <jonbash@protonmail.com>
2020-08-17 12:33:16 -0700

    start setting up carton cell

---
+++ EcoSoapBank/Pickups/NewPickupCartonCell.swift
@@ -0,0 +1,39 @@
+//
+//  NewPickupCartonCell.swift
+//  EcoSoapBank
+//
+//  Created by Jon Bash on 2020-08-17.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+import Combine
+
+
+class NewPickupCartonCell: UITableViewCell {
+    static var reuseIdentifier: String { String(describing: Self.self) }
+    
+    private var viewModel: NewCartonViewModel!
+    private var cancellables: Set<AnyCancellable> = []
+
+    private lazy var cartonLabel = configure(UILabel()) {
+        self.addSubviews(usingAutolayout: true, $0)
+        NSLayoutConstraint.activate([
+            $0.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
+            
+        ])
+    }
+
+    override func prepareForReuse() {
+        self.viewModel = nil
+        cancellables = []
+    }
+
+    func configureCell(for viewModel: NewCartonViewModel) {
+        self.viewModel = viewModel
+
+        viewModel.$carton.sink { carton in
+
+        }.store(in: &cancellables)
+    }
+}
