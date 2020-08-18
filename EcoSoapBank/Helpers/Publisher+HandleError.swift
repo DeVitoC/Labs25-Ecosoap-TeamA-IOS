dfa0a1030aa1d3bccfe50029b24983b025a5002f
Jon Bash <jonbash@protonmail.com>
2020-08-18 09:57:19 -0700

    add error handler for publishers

---
+++ EcoSoapBank/Helpers/Publisher+HandleError.swift
@@ -0,0 +1,29 @@
+//
+//  Publisher+HandleError.swift
+//  EcoSoapBank
+//
+//  Created by Jon Bash on 2020-08-18.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import Combine
+
+
+extension Publisher {
+    func handleError(_ handle: @escaping (Error) -> Void) -> AnyPublisher<Output, Never> {
+        self.handleEvents(
+            receiveSubscription: nil,
+            receiveOutput: nil,
+            receiveCompletion: { completion in
+                if case .failure(let error) = completion {
+                    handle(error)
+                }
+            },
+            receiveCancel: nil,
+            receiveRequest: nil)
+            .map { output -> Output? in output }
+            .replaceError(with: nil)
+            .compactMap { $0 }
+            .eraseToAnyPublisher()
+    }
+}
