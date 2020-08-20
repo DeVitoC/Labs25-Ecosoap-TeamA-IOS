e68fe655bb963cada734c90344ddef9898aea2e0
Jon Bash <jonbash@protonmail.com>
2020-08-20 08:00:29 -0700

    replace SwiftUI PickupDetailView with UIKit

---
+++ EcoSoapBank/Pickups/PickupDetailViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PickupDetailViewController.swift
+//  EcoSoapBank
+//
+//  Created by Jon Bash on 2020-08-19.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import Foundation
