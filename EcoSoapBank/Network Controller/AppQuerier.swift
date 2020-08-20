20e31b3857e220766eea9c16bc60f72d6a544a21
Jon Bash <jonbash@protonmail.com>
2020-08-20 11:08:29 -0700

    add AppQuerier

--- EcoSoapBank/Network Controller/AppQuerier.swift
+++ EcoSoapBank/Network Controller/AppQuerier.swift
@@ -11,3 +11,45 @@ import Foundation
 
 typealias NetworkCompletion<T> = (Result<T, Error>) -> Void
 
+
+class AppQuerier {
+    private var networkService: GraphQLController
+    private var token: String
+
+    init(networkService: GraphQLController, token: String) {
+        self.networkService = networkService
+        self.token = token
+    }
+
+    private func query<T: Decodable>(
+        expecting resultType: T.Type,
+        query: String,
+        options: [String: Any] = [:],
+        completion: @escaping NetworkCompletion<T>
+    ) {
+        var variables: [String: Any] = ["token": token]
+        options.forEach { key, value in variables[key] = value }
+        networkService.queryRequest(T.self, query: query, completion: completion)
+    }
+}
+
+extension AppQuerier: UserDataProvider {
+    func logIn(_ completion: @escaping NetworkCompletion<User>) {
+        query(expecting: User.self,
+              query: GraphQLMutations.login,
+              completion: completion)
+    }
+}
+
+extension AppQuerier: PickupDataProvider {
+    func fetchAllPickups(_ completion: @escaping NetworkCompletion<[Pickup]>) {
+        fatalError("TODO")
+    }
+
+    func schedulePickup(
+        _ pickupInput: Pickup.ScheduleInput,
+        completion: @escaping NetworkCompletion<Pickup.ScheduleResult>
+    ) {
+        fatalError("TODO")
+    }
+}
