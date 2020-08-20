d46b23c26589a702fca11e614b21cdc25a1fe42a
Jon Bash <jonbash@protonmail.com>
2020-08-20 11:44:40 -0700

    add UserFacingError

---
+++ EcoSoapBank/Helpers/UserFacingError.swift
@@ -0,0 +1,16 @@
+//
+//  UserFacingError.swift
+//  EcoSoapBank
+//
+//  Created by Jon Bash on 2020-08-20.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import Foundation
+
+
+/// An error that may be presented to the user.
+protocol UserFacingError: Error {
+    /// A short description of the error to be displayed to the user. Should be short and layperson-friendly.
+    var userFacingDescription: String { get }
+}
