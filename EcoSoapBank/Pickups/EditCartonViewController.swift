75cac3bb48bafad43bc6889300c4ea591f5f48e7
Jon Bash <jonbash@protonmail.com>
2020-08-21 10:01:07 -0700

    add EditCartonVC

--- EcoSoapBank/Pickups/EditCartonViewController.swift
+++ EcoSoapBank/Pickups/EditCartonViewController.swift
@@ -6,4 +6,22 @@
 //  Copyright © 2020 Spencer Curtis. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+
+class EditCartonViewController: UIViewController {
+    let label = configure(UILabel()) {
+        $0.text = "Hello, world!"
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.constrainNewSubviewToSides(label)
+        NSLayoutConstraint.activate([
+            view.widthAnchor.constraint(equalToConstant: 300),
+            view.heightAnchor.constraint(equalToConstant: 300)
+        ])
+        preferredContentSize =
+            view.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
+    }
+}
