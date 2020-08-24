2cbb73bbfac9219d9eff39e0f4aed76e27e3f745
Shawn Gee <shawngee@gmail.com>
2020-08-24 11:11:25 -0400

    add impact cell tests

--- EcoSoapBankTests/Impact Tests/ImpactCellTests.swift
+++ EcoSoapBankTests/Impact Tests/ImpactCellTests.swift
@@ -6,28 +6,46 @@
 //  Copyright © 2020 Spencer Curtis. All rights reserved.
 //
 
+@testable import EcoSoapBank
 import XCTest
 
 class ImpactCellTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var cell: ImpactCell!
+    var titleLabel: UILabel!
+    var subtitleLabel: UILabel!
+    var imageView: ESBCircularImageView!
+    
+    override func setUp() {
+        super.setUp()
+        cell = ImpactCell()
+        titleLabel = cell.contentView.subviews[0] as? UILabel
+        subtitleLabel = cell.contentView.subviews[1] as? UILabel
+        imageView = cell.contentView.subviews[2] as? ESBCircularImageView
+        
+        cell.viewModel = ImpactCellViewModel(amount: 10,
+                                             unit: .people,
+                                             subtitle: "This is the subtitle",
+                                             image: .people)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testInit() {
+        print(cell.subviews)
+        XCTAssertNotNil(cell)
+        XCTAssertNotNil(titleLabel)
+        XCTAssertNotNil(subtitleLabel)
+        XCTAssertNotNil(imageView)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testTitleLabelIsProperlySet() {
+        XCTAssertEqual(titleLabel.text, "10")
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testSubtitleLabelIsProperlySet() {
+        XCTAssertEqual(subtitleLabel.text, "This is the subtitle")
     }
-
+    
+    func testImageIsProperlySet() {
+        XCTAssertEqual(imageView.image, .people)
+    }
 }
