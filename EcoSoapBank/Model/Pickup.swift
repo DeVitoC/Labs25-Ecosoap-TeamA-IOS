df3b42abdef681089a2b2fcf1119107ca1cb060f
Christopher DeVito <christopher.devito@protonmail.com>
2020-08-26 15:57:55 -0400

    In Pickup: Moved custom decoder to extension Added DateFormatter Changed PropertyId to String rather than UUID

--- EcoSoapBank/Model/Pickup.swift
+++ EcoSoapBank/Model/Pickup.swift
@@ -68,54 +68,22 @@ struct Pickup: Identifiable, PickupBaseContainer, Equatable {
         self.property = property
     }
 
-    // Decodes Pickup and the base object
-    init(from decoder: Decoder) throws {
-        let formatter: DateFormatter = {
-            let formatter = DateFormatter()
-            formatter.dateFormat = "yyyy-mm-dd"
-            return formatter
-        }()
-
-        let container = try decoder.container(keyedBy: PickupKeys.self)
-
-        // Decodes all top level values from JSON
-        let id = try container.decode(String.self, forKey: .id)
-        let confirmationCode = try container.decode(String.self, forKey: .confirmationCode)
-        let collectionType = try container.decode(CollectionType.self, forKey: .collectionType)
-        let status = try container.decode(Status.self, forKey: .status)
-        let readyDateString = try container.decode(String.self, forKey: .readyDate)
-        guard let readyDate = formatter.date(from: readyDateString) else { fatalError("unable to formate readyDate") }
-        let pickupDateString = try container.decodeIfPresent(String.self, forKey: .pickupDate)
-        var pickupDate: Date?
-        if let pickupDateStringUnwrapped = pickupDateString {
-            pickupDate = formatter.date(from: pickupDateStringUnwrapped)
-        }
-        let property = try container.decode(Property.self, forKey: .property)
-        let notes = try container.decodeIfPresent(String.self, forKey: .notes)
-
-        // Decodes Cartons based on Carton decoder
-        var cartonsArrayContainer = try container.nestedUnkeyedContainer(forKey: .cartons)
-        let cartonsDict = try cartonsArrayContainer.decode(Pickup.Carton.self)
-
-        let base = Base(collectionType: collectionType, status: status, readyDate: readyDate, pickupDate: pickupDate, notes: notes)
-
-        self.base = base
-        self.cartons = [cartonsDict]
-        self.id = id
-        self.property = property
-        self.confirmationCode = confirmationCode
-    }
-
     static func == (lhs: Pickup, rhs: Pickup) -> Bool {
         lhs.id == rhs.id
             && lhs.confirmationCode == rhs.confirmationCode
             && lhs.property.id == rhs.property.id
     }
+
+    var formatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-mm-dd"
+        return formatter
+    }()
 }
 
 // MARK: - SubTypes
 
-extension Pickup: Decodable {
+extension Pickup {
 
     struct Base: Equatable, Codable {
         let collectionType: CollectionType
@@ -130,7 +98,7 @@ extension Pickup: Decodable {
     struct ScheduleInput: PickupBaseContainer, Encodable {
         let base: Base
 
-        let propertyID: UUID
+        let propertyID: String
         let cartons: [CartonContents]
     }
 
@@ -211,6 +179,75 @@ extension Pickup: Decodable {
         case product
         case percentFull
     }
+}
+
+extension Pickup: Decodable {
+
+    // Decodes Pickup and the base object
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: PickupKeys.self)
+
+        // Decodes all top level values from JSON
+        let id = try container.decode(String.self, forKey: .id)
+        let confirmationCode = try container.decode(String.self, forKey: .confirmationCode)
+        let collectionType = try container.decode(CollectionType.self, forKey: .collectionType)
+        let status = try container.decode(Status.self, forKey: .status)
+
+        // Decode and convert Date string to Date.
+        let readyDateString = try container.decode(String.self, forKey: .readyDate)
+        var readyDate: Date = Date()
+        if let readyDate1 = formatter.date(from: readyDateString) {
+            readyDate = readyDate1
+        } else {
+            let year = readyDateString.dropLast(4)
+            let month = readyDateString.dropFirst(4).dropLast(2)
+            let day = readyDateString.dropFirst(6)
+            if let readyDate2 = formatter.date(from: "\(year)-\(month)-\(day)") {
+                readyDate = readyDate2
+            }
+        }
+
+        let pickupDateString = try container.decodeIfPresent(String.self, forKey: .pickupDate)
+        var pickupDate: Date?
+        if let pickupDateStringUnwrapped = pickupDateString {
+            pickupDate = formatter.date(from: pickupDateStringUnwrapped)
+        }
+        let property = try container.decode(Property.self, forKey: .property)
+        let notes = try container.decodeIfPresent(String.self, forKey: .notes)
+
+        // Decodes Cartons based on Carton decoder
+        var cartonsArrayContainer = try container.nestedUnkeyedContainer(forKey: .cartons)
+        let cartonsDict = try cartonsArrayContainer.decode(Pickup.Carton.self)
+
+        let base = Base(collectionType: collectionType, status: status, readyDate: readyDate, pickupDate: pickupDate, notes: notes)
+
+        self.base = base
+        self.cartons = [cartonsDict]
+        self.id = id
+        self.property = property
+        self.confirmationCode = confirmationCode
+    }
+
+//    func encode(to encoder: Encoder) throws {
+//        // Encode top level values to JSON
+//        var container = encoder.container(keyedBy: PickupKeys.self)
+//        try container.encode(collectionType, forKey: .collectionType)
+//        try container.encode(id, forKey: .id)
+//        try container.encode(confirmationCode, forKey: .confirmationCode)
+//        try container.encode(collectionType, forKey: .collectionType)
+//        try container.encode(status, forKey: .status)
+//        // Encode and convert dates to Date
+//        try container.encode(formatter.string(from: self.readyDate), forKey: .readyDate)
+//        if let pickupDate = self.pickupDate {
+//            try container.encode(formatter.string(from: pickupDate), forKey: .pickupDate)
+//        }
+//        try container.encode(property, forKey: .property)
+//        try container.encodeIfPresent(notes, forKey: .notes)
+//
+//        //Encode Cartons in Carton container
+//        var cartonContainer = container.nestedUnkeyedContainer(forKey: .cartons)
+//        try cartonContainer.encode(cartons)
+//    }
 }
 
 // MARK: Base Container Protocol
