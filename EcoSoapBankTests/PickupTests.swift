8840ecf5e12b5a3e43f40a4594214931435d035c
Jon Bash <jonbash@protonmail.com>
2020-08-26 14:36:06 -0700

    add pickup attribute test

--- EcoSoapBankTests/PickupTests.swift
+++ EcoSoapBankTests/PickupTests.swift
@@ -5,6 +5,7 @@
 //  Created by Jon Bash on 2020-08-11.
 //  Copyright © 2020 Spencer Curtis. All rights reserved.
 //
+// swiftlint:disable force_try
 
 import XCTest
 import SwiftUI
@@ -115,6 +116,50 @@ class PickupTests: XCTestCase {
     func testUserPropertiesTransferredProperly() {
         XCTAssertEqual(pickupCoordinator.schedulePickupVM.properties,
                        pickupController.user.properties)
+    }
+
+    func testSchedulePickupAttributes() throws {
+        let vm = pickupCoordinator.schedulePickupVM
+
+        let in4Days = Date(timeIntervalSinceNow: .days(4))
+        let notes = "This is a note"
+        let property = vm.properties.last
+        XCTAssertNotNil(property)
+
+        vm.readyDate = in4Days
+        vm.notes = notes
+        vm.addCarton()
+        vm.addCarton()
+        vm.addCarton()
+        vm.cartons[0].carton.percentFull = 100
+        vm.cartons[2].carton.percentFull = 50
+        vm.cartons[2].carton.product = .paper
+        vm.removeCarton(atIndex: 1)
+        vm.selectedProperty = try XCTUnwrap(property)
+
+        let exp = newMockExpectation()
+        vm.schedulePickup { result in
+            if case .failure = result {
+                XCTFail("request should not fail")
+            }
+            let pickupResult = try! result.get()
+            let pickup = pickupResult.pickup
+
+            XCTAssertNotNil(pickup)
+            XCTAssertEqual(pickup?.cartons.count, 2)
+            XCTAssertEqual(pickup?.cartons[0].contents?.percentFull, 100)
+            XCTAssertEqual(pickup?.cartons[0].contents?.product, .soap)
+            XCTAssertEqual(pickup?.cartons[1].contents?.percentFull, 50)
+            XCTAssertEqual(pickup?.cartons[1].contents?.product, .paper)
+            XCTAssertEqual(pickup?.notes, notes)
+            XCTAssertEqual(pickup?.property.id, property!.id)
+            XCTAssert(Calendar.current.isDate(pickup!.readyDate,
+                                              equalTo: in4Days,
+                                              toGranularity: .day))
+            exp.fulfill()
+        }
+
+        wait(for: exp)
     }
 }
 
