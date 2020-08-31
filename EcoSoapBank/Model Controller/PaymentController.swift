bf9b5533197eb60809c07bc437e35c835ed9126a
Christopher DeVito <christopher.devito@protonmail.com>
2020-08-31 16:10:35 -0400

    Implemented PaymentController for fetch payments and fetch payments for all property ids.

--- EcoSoapBank/Model Controller/PaymentController.swift
+++ EcoSoapBank/Model Controller/PaymentController.swift
@@ -7,29 +7,57 @@
 //
 
 import Foundation
+import Combine
 
 protocol PaymentDataProvider {
-    func fetchPaymentsByPropertyId(_ completion: @escaping ResultHandler<[Payment]>)
-    func makePayment(_ completion: @escaping ResultHandler<Payment>)
+    func fetchPayments(forPropertyId propertyId: String,
+                       _ completion: @escaping ResultHandler<[Payment]>)
+    func makePayment(_ paymentInput: Payment,
+                     completion: @escaping ResultHandler<Payment>)
 }
 
 class PaymentController {
     private(set) var payments: [Payment] = []
-    private let graphQLController = GraphQLController()
-    var user: User
+    private let dataProvider: PaymentDataProvider
+    private(set) var user: User
+    private var properties: [Property]? {
+        user.properties
+    }
 
-    init(user: User) {
+    init(user: User, dataProvider: PaymentDataProvider) {
         self.user = user
+        self.dataProvider = dataProvider
     }
 
-    func fetchAllPayments() {
+    func fetchAllPayments(forPropertyId propertyId: String) -> Future<[Payment], Error> {
 
-        
+        Future { promise in
+            self.dataProvider.fetchPayments(forPropertyId: propertyId) { [weak self] result in
+                if case .success(let payments) = result {
+                    DispatchQueue.main.async {
+                        self?.payments = payments
+                    }
+                    promise(result)
+                }
+            }
+        }
     }
 
-    func makePayment() {
+    func fetchPaymentsForAllProperties() -> AnyPublisher<[Payment], Error> {
+        guard let properties = properties, !properties.isEmpty else {
+            return Future {
+                $0(.failure(UserError.noProperties))
+            }
+            .eraseToAnyPublisher()
+        }
+        var futures = properties.map { fetchAllPayments(forPropertyId: $0.id)
+        }
+        var combined = futures.popLast()!.eraseToAnyPublisher()
+        while let future = futures.popLast() {
+            combined = combined.append(future).eraseToAnyPublisher()
+        }
 
-
+        return combined
     }
     
 }
