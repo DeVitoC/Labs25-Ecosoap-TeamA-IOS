c2a8b643d26d20524a424ad44e13bc82cb364c7a
Shawn Gee <shawngee@gmail.com>
2020-08-31 16:51:32 -0400

    simplify and unify GraphQL tests

--- EcoSoapBankTests/GraphQLControllerTests.swift
+++ EcoSoapBankTests/GraphQLControllerTests.swift
@@ -10,84 +10,70 @@ import XCTest
 @testable import EcoSoapBank
 
 class GraphQLControllerTests: XCTestCase {
+    
+    var expectation: XCTestExpectation!
+    var graphQLController: GraphQLController!
+    
+    override func setUp() {
+        super.setUp()
+        
+        expectation = XCTestExpectation(description: "Get response from backend API")
+        graphQLController = GraphQLController()
+    }
 
     func testImpactStatsQuerySuccess() {
-        let expectation = XCTestExpectation(description: "Get response from backend API")
-        let graphQLController = GraphQLController()
-
         graphQLController.fetchImpactStats(forPropertyID: "PropertyId1") { result in
 
-            guard let result = try? result.get(),
-                let soapRecycled = result.soapRecycled,
-                let linensRecycled = result.linensRecycled,
-                let bottlesRecycled = result.bottlesRecycled,
-                let paperRecycled = result.paperRecycled,
-                let peopleServed = result.peopleServed,
-                let womenEmployed = result.womenEmployed else {
+            guard let impactStats = try? result.get() else {
                     XCTFail("Unable to get valid impact stats from returned data")
                     return
             }
 
-            XCTAssert(soapRecycled == 11)
-            XCTAssert(linensRecycled == 11)
-            XCTAssert(bottlesRecycled == 11)
-            XCTAssert(paperRecycled == 11)
-            XCTAssert(peopleServed == 11)
-            XCTAssert(womenEmployed == 11)
+            XCTAssertEqual(impactStats.soapRecycled, 11)
+            XCTAssertEqual(impactStats.linensRecycled, 11)
+            XCTAssertEqual(impactStats.bottlesRecycled, 11)
+            XCTAssertEqual(impactStats.paperRecycled, 11)
+            XCTAssertEqual(impactStats.peopleServed, 11)
+            XCTAssertEqual(impactStats.womenEmployed, 11)
 
-            expectation.fulfill()
+            self.expectation.fulfill()
         }
 
         wait(for: [expectation], timeout: 5.0)
     }
 
     func testImpactStatsQueryFailure() {
-        let expectation = XCTestExpectation(description: "Get response from backend API")
-        let graphQLController = GraphQLController()
-
         graphQLController.fetchImpactStats(forPropertyID: "PropertyIId1") { result in
 
             XCTAssertNil(try? result.get())
-            expectation.fulfill()
+            self.expectation.fulfill()
         }
 
         wait(for: [expectation], timeout: 5.0)
     }
 
     func testUserByIdQuerySuccess() {
-        let expectation = XCTestExpectation(description: "Get response from backend API")
-        let graphQLController = GraphQLController()
-
         graphQLController.fetchUser(byID: "UserId1") { result in
 
-            guard let result = try? result.get() else {
+            guard let user = try? result.get() else {
                 XCTFail("Unable to get valid User from returned data")
                 return
             }
-            let id = result.id
-            let firstName = result.firstName
-            let lastName = result.lastName
-            let title = result.title
-            let company = result.company
-            let email = result.email
+            
+            XCTAssertEqual(user.id, "UserId1")
+            XCTAssertEqual(user.firstName, "First Name 1")
+            XCTAssertEqual(user.lastName, "Last Name 1")
+            XCTAssertEqual(user.title, "Title 1")
+            XCTAssertEqual(user.company, "Company 1")
+            XCTAssertEqual(user.email, "Email 1")
 
-            XCTAssert(id == "UserId1")
-            XCTAssert(firstName == "First Name 1")
-            XCTAssert(lastName == "Last Name 1")
-            XCTAssert(title == "Title 1")
-            XCTAssert(company == "Company 1")
-            XCTAssert(email == "Email 1")
-
-            expectation.fulfill()
+            self.expectation.fulfill()
         }
 
         wait(for: [expectation], timeout: 5.0)
     }
 
     func testPickupsByPropertyIdSuccess() {
-        let expectation = XCTestExpectation(description: "Get response from backend API")
-        let graphQLController = GraphQLController()
-
         graphQLController.fetchPickups(forPropertyID: "PropertyId1") { result in
 
             guard let result = try? result.get() else {
@@ -95,60 +81,45 @@ class GraphQLControllerTests: XCTestCase {
                 return
             }
 
-            let id1 = result[0].id
-            let confirmationCode1 = result[0].confirmationCode
-            let collectionType1 = result[0].collectionType
-            let property1ID = result[0].property.id
-            let cartons1ID = result[0].cartons[0].id
-            let notes1 = result[0].notes
+            let firstPickpup = result[0]
             
-            XCTAssert(id1 == "PickupId1")
-            XCTAssert(confirmationCode1 == "PickupConfirmationCode1")
-            XCTAssert(collectionType1 == .generatedLabel)
-            XCTAssert(property1ID == "PropertyId1")
-            XCTAssert(cartons1ID == "PickupCartonId1")
-            XCTAssert(notes1 == "Pickup Notes 1")
+            XCTAssertEqual(firstPickpup.id, "PickupId1")
+            XCTAssertEqual(firstPickpup.confirmationCode, "PickupConfirmationCode1")
+            XCTAssertEqual(firstPickpup.collectionType, .generatedLabel)
+            XCTAssertEqual(firstPickpup.property.id, "PropertyId1")
+            XCTAssertEqual(firstPickpup.cartons[0].id, "PickupCartonId1")
+            XCTAssertEqual(firstPickpup.notes, "Pickup Notes 1")
 
-            expectation.fulfill()
+            self.expectation.fulfill()
         }
 
         wait(for: [expectation], timeout: 5.0)
     }
 
     func testPropertiesByUserIdSuccess() {
-        let expectation = XCTestExpectation(description: "Get response from backend API")
-        let graphQLController = GraphQLController()
-
         graphQLController.fetchProperties(forUserID: "UserId1") { result in
 
             guard let result = try? result.get() else {
                 XCTFail("Unable to get Properties from returned data")
                 return
             }
+            
+            let firstProperty = result[0]
+            
+            XCTAssertEqual(firstProperty.id, "PropertyId1")
+            XCTAssertEqual(firstProperty.propertyType, .hotel)
+            XCTAssertEqual(firstProperty.rooms, 111)
+            XCTAssertEqual(firstProperty.services, [.soap, .linens])
+            XCTAssertEqual(firstProperty.phone, "111-111-1111")
+            XCTAssertEqual(firstProperty.shippingNote, "Shipping note 1.")
 
-            let propertyId = result[0].id
-            let propertyType = result[0].propertyType
-            let rooms = result[0].rooms
-            let services = result[0].services
-            let phone = result[0].phone
-            let shippingNote = result[0].shippingNote
-
-            XCTAssert(propertyId == "PropertyId1")
-            XCTAssert(propertyType == .hotel)
-            XCTAssert(rooms == 111)
-            XCTAssert(services == [.soap, .linens])
-            XCTAssert(phone == "111-111-1111")
-            XCTAssert(shippingNote == "Shipping note 1.")
-
-            expectation.fulfill()
+            self.expectation.fulfill()
         }
 
         wait(for: [expectation], timeout: 5.0)
     }
 
     func testSchedulePickup() {
-        let expectation = XCTestExpectation(description: "Get response from backend API")
-        let graphQLController = GraphQLController()
         let scheuleInput = Pickup.ScheduleInput(base: Pickup.Base(collectionType: .generatedLabel,
                                                                   status: .submitted,
                                                                   readyDate: Date(year: 2020,
@@ -164,16 +135,16 @@ class GraphQLControllerTests: XCTestCase {
         
         graphQLController.schedulePickup(scheuleInput) { result in
             
-            guard let result = try? result.get() else {
+            guard let scheduleResult = try? result.get() else {
                 XCTFail("Unable to get valid Pickup data from returned data")
                 return
             }
             
-            let status = result.pickup?.status
-            let cartonProduct = result.pickup?.cartons[0].contents?.product
-            let cartonPercentFull = result.pickup?.cartons[0].contents?.percentFull
-            let collectionType = result.pickup?.collectionType
-            let label = result.labelURL
+            let status = scheduleResult.pickup?.status
+            let cartonProduct = scheduleResult.pickup?.cartons[0].contents?.product
+            let cartonPercentFull = scheduleResult.pickup?.cartons[0].contents?.percentFull
+            let collectionType = scheduleResult.pickup?.collectionType
+            let label = scheduleResult.labelURL
             
             XCTAssertEqual(status, .submitted)
             XCTAssertEqual(cartonProduct, .soap)
@@ -181,38 +152,28 @@ class GraphQLControllerTests: XCTestCase {
             XCTAssertEqual(collectionType, .generatedLabel)
             XCTAssertEqual(label?.absoluteString, "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf")
             
-            expectation.fulfill()
+            self.expectation.fulfill()
         }
         
         wait(for: [expectation], timeout: 5.0)
     }
     
     func testCancelPickupRequest() {
-        let expectation = XCTestExpectation(description: "Get response from backend API")
-        let graphQLController = GraphQLController()
-
         graphQLController.cancelPickup("PickupId1") { result in
 
-            guard let result = try? result.get() else {
+            guard let pickup = try? result.get() else {
                 XCTFail("Unable to get valid Pickup from returned data")
                 return
             }
+            
+            XCTAssertEqual(pickup.id, "PickupId1")
+            XCTAssertEqual(pickup.confirmationCode, "PickupConfirmationCode1")
+            XCTAssertEqual(pickup.collectionType, .generatedLabel)
+            XCTAssertEqual(pickup.property.id, "PropertyId1")
+            XCTAssertEqual(pickup.cartons[0].id, "PickupCartonId1")
+            XCTAssertEqual(pickup.cartons[0].contents?.percentFull, 11)
 
-            let pickupId = result.id
-            let confirmationCode = result.confirmationCode
-            let collectionType = result.collectionType
-            let propertyId = result.property.id
-            let cartonId = result.cartons[0].id
-            let cartonPercentFull = result.cartons[0].contents?.percentFull
-
-            XCTAssert(pickupId == "PickupId1")
-            XCTAssert(confirmationCode == "PickupConfirmationCode1")
-            XCTAssert(collectionType == .generatedLabel)
-            XCTAssert(propertyId == "PropertyId1")
-            XCTAssert(cartonId == "PickupCartonId1")
-            XCTAssert(cartonPercentFull == 11)
-
-            expectation.fulfill()
+            self.expectation.fulfill()
         }
 
         wait(for: [expectation], timeout: 5.0)
