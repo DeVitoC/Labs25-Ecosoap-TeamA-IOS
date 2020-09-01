e701a8a17e65ea8986ca5cc54616ba21514a2224
Christopher DeVito <christopher.devito@protonmail.com>
2020-09-01 19:00:12 -0400

    Added UIElementInitializers class to create methods to initialize UI elements with standard defaults.

---
+++ EcoSoapBank/Helpers/UIElementInitializers.swift
@@ -0,0 +1,32 @@
+//
+//  UIElementInitializers.swift
+//  EcoSoapBank
+//
+//  Created by Christopher Devito on 9/1/20.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+
+class UIElementInitializers: NSObject {
+
+    /**
+     Create a UILabel with title, frame,alignment, and textColor passed in.
+
+     - Parameter title: **String** that describes the UILabel's text property
+     - Parameter frame: **CGRect** that describes the initial dimensions of the UILabel
+     - Parameter alignment: **NSTextAlignment** that describes the UILabel's  textAlignment property
+     - Parameter textColor: **UIColor** that describes the UILabel's textColor property
+     - Returns: A **UILabel** set up to the specifications passed in.
+     */
+    func createLabel(_ title: String, frame: CGRect, alignment: NSTextAlignment, textColor: UIColor = .white) -> UILabel {
+        let label = UILabel(frame: frame)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = title
+        label.textAlignment = alignment
+        label.textColor = textColor
+
+        return label
+    }
+
+}
