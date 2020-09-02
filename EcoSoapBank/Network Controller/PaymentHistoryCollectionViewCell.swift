3125905f761455a1318d87179919dc0a17c62ad1
Christopher DeVito <christopher.devito@protonmail.com>
2020-09-02 14:04:10 -0400

    Unwrapped several properties of payment and added to subview

--- EcoSoapBank/Network Controller/PaymentHistoryCollectionViewCell.swift
+++ EcoSoapBank/Network Controller/PaymentHistoryCollectionViewCell.swift
@@ -13,14 +13,25 @@ class PaymentHistoryCollectionViewCell: UICollectionViewCell {
     private lazy var uiInitializers = UIElementInitializers()
 
     func setupUI() {
-        guard let payment = payment else { return }
-        let invoiceString = "\(payment.invoicePeriodStartDate) - \(payment.invoicePeriodEndDate)"
+        guard let payment = payment,
+            let invoicePeriodStartDate = payment.invoicePeriodStartDate,
+            let invoicePeriodEndDate = payment.invoicePeriodEndDate,
+            let invoiceCode = payment.invoiceCode else {
+                return
+        }
+        let invoiceString = "\(invoicePeriodStartDate) - \(invoicePeriodEndDate)"
         let invoicePeriodLabel = uiInitializers.createLabel(invoiceString, frame: CGRect(origin: .zero, size: .zero), alignment: .left)
         let amountDueLabel = uiInitializers.createLabel("\(payment.amountDue)", frame: .zero, alignment: .left)
         let amountPaidLabel = uiInitializers.createLabel("\(payment.amountPaid)", frame: .zero, alignment: .left)
         let paymentMethodLabel = uiInitializers.createLabel("\(payment.paymentMethod)", frame: .zero, alignment: .left)
-        let invoiceNumberLabel = uiInitializers.createLabel("\(payment.invoiceCode)", frame: .zero, alignment: .left)
+        let invoiceNumberLabel = uiInitializers.createLabel("\(invoiceCode)", frame: .zero, alignment: .left)
         let paymentDateLabel = uiInitializers.createLabel("\(payment.date)", frame: .zero, alignment: .left)
-        
+
+        addSubview(invoicePeriodLabel)
+        addSubview(amountDueLabel)
+        addSubview(amountPaidLabel)
+        addSubview(paymentMethodLabel)
+        addSubview(invoiceNumberLabel)
+        addSubview(paymentDateLabel)
     }
 }
