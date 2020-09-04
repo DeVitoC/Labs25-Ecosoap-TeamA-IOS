f438e441df34cfbd7e1454529c60db96fb19304c
Jon Bash <jonbash@protonmail.com>
2020-09-04 12:10:29 -0700

    add folder for profile VMs

---
+++ EcoSoapBankTests/ProfileTests.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileTests.swift
+//  EcoSoapBankTests
+//
+//  Created by Jon Bash on 2020-09-04.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import XCTest
+
+class ProfileTests: XCTestCase {
+    
+}
