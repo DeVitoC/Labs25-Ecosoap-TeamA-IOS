3e0c854a70e1af20a4404e995f4954c6464a767f
Jon Bash <jonbash@protonmail.com>
2020-09-04 12:42:02 -0700

    set up ProfileTests

--- EcoSoapBankTests/ProfileTests.swift
+++ EcoSoapBankTests/ProfileTests.swift
@@ -7,7 +7,36 @@
 //
 
 import XCTest
+@testable import EcoSoapBank
 
 class ProfileTests: XCTestCase {
-    
+    var user: User!
+    var dataProvider: UserDataProvider!
+    var userController: UserController!
+    var coordinator: ProfileCoordinator!
+
+    var badUser: User!
+
+    var mainVM: MainProfileViewModel { coordinator.profileVM }
+
+    override func setUp() {
+        super.setUp()
+        self.user = .placeholder()
+        self.dataProvider = MockLoginProvider()
+        self.userController = UserController(dataLoader: dataProvider)
+        self.coordinator = ProfileCoordinator(user: user,
+                                              userController: userController)
+
+        self.badUser = User(
+            id: "",
+            firstName: "",
+            middleName: nil,
+            lastName: "",
+            title: nil,
+            company: nil,
+            email: "",
+            phone: nil,
+            skype: nil,
+            properties: nil)
+    }
 }
