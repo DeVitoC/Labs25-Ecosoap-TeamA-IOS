c31c891dbb82340dc002c372c8fea94131485fee
Christopher DeVito <christopher.devito@protonmail.com>
2020-09-08 13:13:44 -0400

    Attempted to add navigation bar. Not fully functioning yet.

--- EcoSoapBankTests/PaymentHistoryViewController.swift
+++ EcoSoapBankTests/PaymentHistoryViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class PaymentHistoryViewController: UIViewController {
 
     var paymentController: PaymentController?
-    private var paymentCollectionView: UICollectionView = UICollectionView(frame: .infinite, collectionViewLayout: UICollectionViewFlowLayout())
+    private var paymentCollectionView: UICollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
     private var payments: [Payment] = [] {
         didSet {
             self.paymentCollectionView.reloadData()
@@ -20,6 +20,14 @@ class PaymentHistoryViewController: UIViewController {
 
     override func loadView() {
         view = BackgroundView()
+    }
+
+    override func viewWillLayoutSubviews() {
+        let width = self.view.frame.width
+        let navigationBar: UINavigationBar = UINavigationBar(frame: CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: width, height: 44)))
+        view.addSubview(navigationBar)
+        let navBarTitle = UINavigationItem(title: "Payment History")
+        navigationBar.setItems([navBarTitle], animated: false)
     }
 
     override func viewDidLoad() {
@@ -42,11 +50,15 @@ class PaymentHistoryViewController: UIViewController {
         let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
 
-        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(44.0))
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(150))
         let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
 
         let section = NSCollectionLayoutSection(group: group)
         return UICollectionViewCompositionalLayout(section: section)
+    }
+
+    func setupNavBar() {
+//        view.add
     }
 
     func setupCollectionView() {
@@ -56,9 +68,9 @@ class PaymentHistoryViewController: UIViewController {
         paymentCollectionView.delegate = self
         paymentCollectionView.translatesAutoresizingMaskIntoConstraints = false
         paymentCollectionView.register(PaymentHistoryCollectionViewCell.self, forCellWithReuseIdentifier: "PaymentCell")
-        paymentCollectionView.backgroundColor = .clear
+        paymentCollectionView.backgroundColor = .white
         NSLayoutConstraint.activate([
-            paymentCollectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            paymentCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 44),
             paymentCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             paymentCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             paymentCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
@@ -81,8 +93,7 @@ extension PaymentHistoryViewController: UICollectionViewDataSource {
             withReuseIdentifier: "PaymentCell",
             for: indexPath) as? PaymentHistoryCollectionViewCell
             else { return UICollectionViewCell() }
-
+        cell.payment = payments[indexPath.row]
         return cell
     }
-
 }
