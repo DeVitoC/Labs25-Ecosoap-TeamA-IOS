994c89dfb9be6328aecb3e5ace44a8f524696329
Shawn Gee <shawngee@gmail.com>
2020-09-08 10:24:16 -0400

    remove gradient from button, increase font size

--- EcoSoapBank/Custom Views/ESBButton.swift
+++ EcoSoapBank/Custom Views/ESBButton.swift
@@ -11,12 +11,12 @@ import UIKit
 class ESBButton: UIButton {
     enum ColorScheme {
         case greenOnWhite
-        case primaryOnGradient
+        case primaryOnBlue
     }
     
     // MARK: - Public Properties
     
-    var colorScheme: ColorScheme = .primaryOnGradient {
+    var colorScheme: ColorScheme = .primaryOnBlue {
         didSet { updateColors() }
     }
     
@@ -53,20 +53,10 @@ class ESBButton: UIButton {
                 UIColor(light: UIColor.esbGreen.adjustingBrightness(by: -0.1),
                         dark: UIColor.esbGreen.adjustingBrightness(by: -0.3)),
                 for: .normal)
-            backgroundColor = UIColor.white
-            backgroundGradient?.removeFromSuperview()
-            backgroundGradient = nil
-        case .primaryOnGradient:
+            backgroundColor = .white
+        case .primaryOnBlue:
             setTitleColor(UIColor.label, for: .normal)
-            backgroundGradient = configure(GradientView()) {
-                $0.colors = [UIColor.esbGreen.orInverse(),
-                             UIColor.downyBlue.orInverse()]
-                $0.startPoint = CGPoint(x: 0, y: 0)
-                $0.endPoint = CGPoint(x: 1, y: 0)
-            }
-            addSubview(backgroundGradient!)
-            sendSubviewToBack(backgroundGradient!)
-            backgroundGradient?.fillSuperview()
+            backgroundColor = UIColor.downyBlue.orAdjustingBrightness(by: -0.3)
         }
     }
     
@@ -74,7 +64,7 @@ class ESBButton: UIButton {
         updateColors()
         clipsToBounds = true
         layer.cornerRadius = 8.0
-        titleLabel?.font = .muli( style: .body, typeface: .semiBold)
+        titleLabel?.font = .muli( style: .title3, typeface: .semiBold)
         contentEdgeInsets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
     }
 }
