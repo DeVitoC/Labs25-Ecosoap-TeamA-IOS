e8af7f34fee0ada0bff8932f561a4840aae72f23
Christopher DeVito <christopher.devito@protonmail.com>
2020-09-09 12:25:06 -0400

    Changed PaymentCoordinator to embed in nav controller.

--- EcoSoapBank/Payment/PaymentCoordinator.swift
+++ EcoSoapBank/Payment/PaymentCoordinator.swift
@@ -9,14 +9,14 @@
 import UIKit
 
 class PaymentCoordinator: FlowCoordinator {
-
+    
     init(user: User, dataProvider: PaymentDataProvider) {
-        rootVC = configure(PaymentHistoryViewController()) {
-            $0.paymentController = PaymentController(user: user, dataProvider: dataProvider)
-        }
+        paymentVC.paymentController = PaymentController(user: user,
+                                                        dataProvider: dataProvider)
     }
 
-    let rootVC: PaymentHistoryViewController
+    let rootVC = ESBNavigationController()
+    let paymentVC = PaymentHistoryViewController()
 
     func start() {
         let payment = UIImage(
@@ -24,5 +24,6 @@ class PaymentCoordinator: FlowCoordinator {
             withConfiguration: UIImage.SymbolConfiguration(pointSize: 22, weight: .regular)
         )
         rootVC.tabBarItem = UITabBarItem(title: "Payments", image: payment, tag: 0)
+        rootVC.pushViewController(paymentVC, animated: false)
     }
 }
