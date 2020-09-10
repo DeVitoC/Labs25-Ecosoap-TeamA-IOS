20bb2d092d00a29a202257ba90e8eb6110264727
Shawn Gee <shawngee@gmail.com>
2020-09-10 13:00:58 -0400

    add carton view

---
+++ EcoSoapBank/Pickups/Views/CartonView.swift
@@ -0,0 +1,21 @@
+//
+//  CartonView.swift
+//  EcoSoapBank
+//
+//  Created by Shawn Gee on 9/10/20.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+
+class CartonView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
