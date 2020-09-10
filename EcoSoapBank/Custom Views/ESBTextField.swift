e53b1a2609de342642f2e3a1e6eb5023dc764dad
Shawn Gee <shawngee@gmail.com>
2020-09-10 19:54:28 -0400

    set background color in ESBTextField

--- EcoSoapBank/Custom Views/ESBTextField.swift
+++ EcoSoapBank/Custom Views/ESBTextField.swift
@@ -26,6 +26,7 @@ class ESBTextField: UITextField, ESBBordered {
     }
     
     private func setUp() {
+        backgroundColor = .systemBackground
         borderStyle = .none
         configureBorder()
     }
