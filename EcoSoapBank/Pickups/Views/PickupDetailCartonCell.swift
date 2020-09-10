20b41a613532b5dbd21bce74cf786b0a8e6523d4
Shawn Gee <shawngee@gmail.com>
2020-09-10 19:55:34 -0400

    implement PickupDetailCartonCell

--- EcoSoapBank/Pickups/Views/PickupDetailCartonCell.swift
+++ EcoSoapBank/Pickups/Views/PickupDetailCartonCell.swift
@@ -9,5 +9,27 @@
 import UIKit
 
 class PickupDetailCartonCell: UICollectionViewCell {
+    var carton: Pickup.Carton? { didSet { updateViews() } }
     
+    @IBOutlet private var productLabel: UILabel! {
+        didSet {
+            productLabel.font = UIFont.muli(style: .callout, typeface: .regular)
+            productLabel.textColor = UIColor.label
+        }
+    }
+    @IBOutlet private var cartonView: CartonView!
+    @IBOutlet private var percentFullLabel: UILabel! {
+        didSet {
+            percentFullLabel.font = UIFont.muli(style: .caption1, typeface: .semiBold)
+            percentFullLabel.textColor = UIColor.codGrey.orInverse()
+        }
+    }
+    
+    private func updateViews() {
+        guard let cartonContents = carton?.contents else { return }
+        
+        productLabel.text = cartonContents.product.rawValue.capitalized
+        cartonView.percentFull = cartonContents.percentFull
+        percentFullLabel.text = ("\(cartonContents.percentFull)%")
+    }
 }
