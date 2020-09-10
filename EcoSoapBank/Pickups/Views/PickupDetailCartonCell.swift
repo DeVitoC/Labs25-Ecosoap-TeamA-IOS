42949ddd97a0f91c6a01dc68f9ff278af93d4ecb
Shawn Gee <shawngee@gmail.com>
2020-09-10 16:10:07 -0400

    add PickupDetailCartonCell

---
+++ EcoSoapBank/Pickups/Views/PickupDetailCartonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PickupDetailCartonCell.swift
+//  EcoSoapBank
+//
+//  Created by Shawn Gee on 9/10/20.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+
+class PickupDetailCartonCell: UICollectionViewCell {
+    
+}
