2eab3a6b7bc7534194acb23e18d49e9fc4aa5ba8
Jon Bash <jonbash@protonmail.com>
2020-09-11 08:00:56 -0700

    add SwiftUI alert stuff

---
+++ EcoSoapBank/Helpers/SwiftUI/Alert+ErrorMessage.swift
@@ -0,0 +1,42 @@
+//
+//  Alert+ErrorMessage.swift
+//  EcoSoapBank
+//
+//  Created by Jon Bash on 2020-09-10.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import SwiftUI
+
+
+extension Alert {
+    init(_ errorMessage: ErrorMessage,
+         primaryButton: Button? = nil,
+         secondaryButton: Button? = nil
+    ) {
+        let title = Text(errorMessage.title)
+        let message = Text(errorMessage.message)
+
+        if let primary = primaryButton {
+            if let secondary = secondaryButton {
+                self.init(title: title,
+                          message: message,
+                          primaryButton: primary,
+                          secondaryButton: secondary)
+            } else {
+                self.init(title: title, message: message, dismissButton: primary)
+            }
+        } else {
+            self.init(title: title, message: message)
+        }
+    }
+
+    init(_ error: Error?,
+         primaryButton: Button? = nil,
+         secondaryButton: Button? = nil
+    ) {
+        self.init(ErrorMessage(error),
+                  primaryButton: primaryButton,
+                  secondaryButton: secondaryButton)
+    }
+}
