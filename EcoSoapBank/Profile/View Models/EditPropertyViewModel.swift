ee507aacabec6b5c8e1480471b72f8ea3e62a93b
Jon Bash <jonbash@protonmail.com>
2020-09-11 09:50:54 -0700

    move editable info into models

--- EcoSoapBank/Profile/View Models/EditPropertyViewModel.swift
+++ EcoSoapBank/Profile/View Models/EditPropertyViewModel.swift
@@ -6,62 +6,32 @@
 //  Copyright © 2020 Spencer Curtis. All rights reserved.
 //
 
-import Foundation
+import Combine
+import SwiftUI
 
 
 class EditPropertyViewModel: ObservableObject, Identifiable {
     @Published var propertyInfo: EditablePropertyInfo
     @Published var useShippingAddressForBilling: Bool = false
-    var property: Property
+    @Published var error: Error?
+    @Binding var isActive: Bool
+    lazy var didFail = Binding(
+        get: { [weak self] in self?.error != nil },
+        set: { [weak self] newFail in if !newFail { self?.error = nil } })
+    let property: Property
 
     var id: String { property.id }
     let propertyTypes: [Property.PropertyType] = Property.PropertyType.allCases
+
+    private let userController: UserController
     
-    init(_ property: Property) {
+    init(_ property: Property, isActive: Binding<Bool>, userController: UserController) {
         self.propertyInfo = EditablePropertyInfo(property)
         self.property = property
+        self._isActive = isActive
+        self.userController = userController
     }
 
     func commitChanges() {
-        
-    }
-}
-
-
-struct EditablePropertyInfo: Encodable {
-    let id: String?
-    var name: String
-    var propertyType: Property.PropertyType
-    var billingAddress: EditableAddressInfo
-    var shippingAddress: EditableAddressInfo
-    var phone: String
-
-    init(_ property: Property?) {
-        self.id = property?.id
-        self.name = property?.name ?? ""
-        self.propertyType = property?.propertyType ?? .hotel
-        self.billingAddress = EditableAddressInfo(property?.billingAddress)
-        self.shippingAddress = EditableAddressInfo(property?.shippingAddress)
-        self.phone = property?.phone ?? ""
-    }
-}
-
-struct EditableAddressInfo: Encodable {
-    var address1: String
-    var address2: String
-    var address3: String
-    var city: String
-    var state: String
-    var postalCode: String
-    var country: String
-
-    init(_ address: Address?) {
-        self.address1 = address?.address1 ?? ""
-        self.address2 = address?.address2 ?? ""
-        self.address3 = address?.address3 ?? ""
-        self.city = address?.city ?? ""
-        self.state = address?.state ?? ""
-        self.postalCode = address?.postalCode ?? ""
-        self.country = address?.country ?? ""
     }
 }
