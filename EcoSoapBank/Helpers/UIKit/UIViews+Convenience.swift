289120a046db666b97557540ace552f9f1ce11ac
Jon Bash <jonbash@protonmail.com>
2020-09-14 09:12:53 -0700

    add arrangedsubviews to uistackview init

--- EcoSoapBank/Helpers/UIKit/UIViews+Convenience.swift
+++ EcoSoapBank/Helpers/UIKit/UIViews+Convenience.swift
@@ -40,12 +40,14 @@ extension UIStackView {
         axis: NSLayoutConstraint.Axis = .vertical,
         alignment: UIStackView.Alignment = .fill,
         distribution: UIStackView.Distribution = .fill,
-        spacing: CGFloat = 8
+        spacing: CGFloat = 8,
+        arrangedSubviews: [UIView] = []
     ) {
         self.init()
         self.axis = axis
         self.alignment = alignment
         self.distribution = distribution
         self.spacing = spacing
+        arrangedSubviews.forEach(addArrangedSubview(_:))
     }
 }
