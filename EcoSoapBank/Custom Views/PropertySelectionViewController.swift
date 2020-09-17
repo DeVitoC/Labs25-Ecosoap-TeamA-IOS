d7fccdfec1b85e17a7487430934344f6d8525b4a
Shawn Gee <shawngee@gmail.com>
2020-09-17 11:37:57 -0400

    add property selection vc

---
+++ EcoSoapBank/Custom Views/PropertySelectionViewController.swift
@@ -0,0 +1,31 @@
+
+//
+//  PropertySelectionViewController.swift
+//  EcoSoapBank
+//
+//  Created by Shawn Gee on 9/17/20.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+
+class PropertySelectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
