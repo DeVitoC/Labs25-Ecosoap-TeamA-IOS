3f72f563a91de10d44d21a3bb460adbecec8df7b
Jon Bash <jonbash@protonmail.com>
2020-09-21 15:11:58 -0700

    conform GraphQLError to LocalizedError

--- EcoSoapBank/Networking/GraphQLError.swift
+++ EcoSoapBank/Networking/GraphQLError.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-enum GraphQLError: Error {
+enum GraphQLError: LocalizedError {
     case noData
     case invalidData
     case noToken
@@ -16,4 +16,25 @@ enum GraphQLError: Error {
     case backendMessages([String])
     case encodingError(Error)
     case decodingError(Error)
+
+    var errorDescription: String? {
+        let dataMessage = "Error with data"
+        let unknown = "Unknown error"
+        switch self {
+        case .noData, .invalidData:
+            return dataMessage
+        case .noToken:
+            return "Error with sign-in info"
+        case .backendMessages:
+            return "Server error"
+        case .encodingError(let other), .decodingError(let other):
+            return (other as? LocalizedError)?.errorDescription ?? dataMessage
+        default:
+            return unknown
+        }
+    }
+
+    var recoverySuggestion: String? {
+        "Try again a little later. Contact us if this happens repeatedly."
+    }
 }
