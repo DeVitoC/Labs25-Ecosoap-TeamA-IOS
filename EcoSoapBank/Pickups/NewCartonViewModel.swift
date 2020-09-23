32cdb1dc9e6128c3fd976966e71bfa909be50175
Christopher DeVito <christopher.devito@protonmail.com>
2020-09-23 17:45:07 -0400

    Added Documentation in NewCartonViewModel

--- EcoSoapBank/Pickups/NewCartonViewModel.swift
+++ EcoSoapBank/Pickups/NewCartonViewModel.swift
@@ -9,19 +9,29 @@
 import Foundation
 
 
+/// The ViewModel that controlls the individual Cartons in the SchedulePickupViewModel
 class NewCartonViewModel {
     @Published var carton: Pickup.CartonContents
 
+    /// Initializes a **Carton** with the contents entered
+    /// - Parameter carton: **Pickup.CartonContents** object with the type and percent full of the carton to initialize
     init(carton: Pickup.CartonContents) {
         self.carton = carton
     }
 }
 
 extension NewCartonViewModel: Hashable {
+    /// <#Description#>
+    /// - Parameters:
+    ///   - lhs: <#lhs description#>
+    ///   - rhs: <#rhs description#>
+    /// - Returns: <#description#>
     static func == (lhs: NewCartonViewModel, rhs: NewCartonViewModel) -> Bool {
         lhs === rhs && lhs.carton == rhs.carton
     }
 
+    /// <#Description#>
+    /// - Parameter hasher: <#hasher description#>
     func hash(into hasher: inout Hasher) {
         hasher.combine(ObjectIdentifier(self))
     }
