122f019642bc61282e419e55c27cea95bc5a79a7
Christopher DeVito <christopher.devito@protonmail.com>
2020-09-24 13:20:21 -0400

    Added documentation and comments to the PropertySelectorCell.

--- EcoSoapBank/Features/PropertySelection/PropertySelectorCell.swift
+++ EcoSoapBank/Features/PropertySelection/PropertySelectorCell.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+/// Sets up the view for each cell in the PropertySelector.
 class PropertySelectorCell: UITableViewCell {
     
     let label = configure(UILabel()) {
@@ -15,7 +16,11 @@ class PropertySelectorCell: UITableViewCell {
         $0.font = .montserrat(ofSize: 17, typeface: .medium)
         $0.textColor = .downyBlue
     }
-    
+
+    /// Initializes and configures the cell
+    /// - Parameters:
+    ///   - style: The **UITableViewCell.CellStyle** to use for the cell.
+    ///   - reuseIdentifier: The reuseIdentifier for the cell
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setUp()
@@ -25,7 +30,8 @@ class PropertySelectorCell: UITableViewCell {
         super.init(coder: aDecoder)
         setUp()
     }
-    
+
+    /// Adds the label to the cell and configures it to display the specific property
     private func setUp() {
         addSubviewsUsingAutolayout(label)
         label.fillSuperview()
